89ce8e6c3cbe36daf4288344503f06ad21ab7843
Mahesh Titame <maheshtitame71@gmail.com>
2026-09-08 23:11:52 +0530

    Add Vercel Analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes/AppRoutes'
 import React from 'react'
+import { Analytics } from "@vercel/analytics/react";
 
 
 function App() {
   return (
     <BrowserRouter>
     <AppRoutes/>
+    <Analytics />
     </BrowserRouter>
   )
 }
